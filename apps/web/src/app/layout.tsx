d8005b7476b2c49b11b2851cce4021d5eb8349a4
Sudip Biswas <sudipbiswas2142@gmail.com>
2026-03-04 21:50:44 +0530

    chore: frontend setup

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Antisocial',
+  description: 'The most antisocial social media application',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans bg-background text-foreground`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main className="min-h-dvh">
+      <section className="max-w-lg space-y-6 py-12 px-6 mx-auto">
+        <div className="space-y-4 *:tracking-tight">
+          <h1 className="text-3xl font-bold">The Anti-Social Social Network</h1>
+          <p className="text-neutral-600 font-medium">
+            Anti-Social lets you post and share notes, links or images by email subject only. From
+            any email account.
+          </p>
+        </div>
+        <button className="px-4 py-2 rounded-full bg-neutral-200/75 text-sm font-medium outline-none inline-flex items-center justify-center">
+          Create account / $10
+        </button>
+      </section>
+    </main>
+  );
+}
