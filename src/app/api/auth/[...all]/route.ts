e5c9706e5f1cefef04526260bebb73fb3be3ad3f
Sudip Biswas <sudipbiswas2142@gmail.com>
2026-03-06 21:12:31 +0530

    fix: refactor auth handling to use createAuth function for integrating cloudflare context

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,17 @@
-import { auth } from '@/lib/auth/config';
+import type { NextRequest } from 'next/server';
 import { toNextJsHandler } from 'better-auth/next-js';
+import { getCloudflareContext } from '@opennextjs/cloudflare';
 
-export const { POST, GET } = toNextJsHandler(auth);
+import { createAuth } from '@/lib/auth/config';
+
+export async function GET(req: NextRequest) {
+  const cf = await getCloudflareContext({ async: true });
+  const auth = createAuth(cf.env.D1_DATABASE);
+  return toNextJsHandler(auth).GET(req);
+}
+
+export async function POST(req: NextRequest) {
+  const cf = await getCloudflareContext({ async: true });
+  const auth = createAuth(cf.env.D1_DATABASE);
+  return toNextJsHandler(auth).POST(req);
+}
--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -1,28 +1,27 @@
 import { betterAuth } from 'better-auth';
 import { emailOTP } from 'better-auth/plugins';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { getCloudflareContext } from '@opennextjs/cloudflare';
 
 import { getDb } from '@/db';
 import config from '@/lib/config';
 import * as schema from '@/db/schema';
 
-const cf = getCloudflareContext();
-
-export const auth = betterAuth({
-  appName: config.appName,
-  baseURL: config.baseURL,
-  secret: config.auth.secret,
-  database: drizzleAdapter(getDb(cf.env.D1_DATABASE), {
-    provider: 'sqlite',
-    schema,
-    usePlural: true,
-  }),
-  plugins: [
-    emailOTP({
-      async sendVerificationOTP(data, ctx) {
-        // TODO: Send verification OTP to email
-      },
+export function createAuth(d1: D1Database) {
+  return betterAuth({
+    appName: config.appName,
+    baseURL: config.baseURL,
+    secret: config.auth.secret,
+    database: drizzleAdapter(getDb(d1), {
+      provider: 'sqlite',
+      schema,
+      usePlural: true,
     }),
-  ],
-});
+    plugins: [
+      emailOTP({
+        async sendVerificationOTP(data, ctx) {
+          // TODO: Send verification OTP to email
+        },
+      }),
+    ],
+  });
+}
